3224397c73f5a82ad9a3792d060adc51ac1cc330
Joseph MIddleton <j.eg.middleton5@gmail.com>
2026-04-27 17:30:17 +0100

    Follow python plugins to compile them.

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ PY_IPA="/usr/lib/python3.12/site-packages/ipaserver"
 cp -r ./js/mailoptions $JS_IPA/plugins/
 cp -r ./python/* $PY_IPA/plugins/
 
+cd $PY_IPA/plugins/
 python -m compileall mail*
 python -O -m compileall mail*
