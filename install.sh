a88468bae8cae6cb41b751eb44134889f260abb7
Joseph MIddleton <j.eg.middleton5@gmail.com>
2026-04-27 17:28:13 +0100

    Changes to JS_IPA path

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-JS_IPA="/usr/lib/python3.12/site-packages/ipaserver"
+JS_IPA="/usr/share/ipa/ui/js"
 PY_IPA="/usr/lib/python3.12/site-packages/ipaserver"
 
-cp -r ./js/mailoptions $JS_IPA/plugins
+cp -r ./js/mailoptions $JS_IPA/plugins/
 cp -r ./python/* $PY_IPA/plugins/
 
 python -m compileall mail*
