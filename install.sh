e39c12668cb5933c9944d3d7afc7260631e4601e
Joseph MIddleton <j.eg.middleton5@gmail.com>
2026-07-21 13:26:21 +0100

    Updated python version

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 JS_IPA="/usr/share/ipa/ui/js"
-PY_IPA="/usr/lib/python3.12/site-packages/ipaserver"
+PY_IPA="/usr/lib/python3.13/site-packages/ipaserver"
 
 cp -r ./js/mailoptions $JS_IPA/plugins/
 cp -r ./python/* $PY_IPA/plugins/
